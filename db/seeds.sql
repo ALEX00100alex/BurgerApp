3c86598ed2b28f520bb9b7e75a48b9f7f9fe0dcb
Michael Alexander <michael.m.alexander@outlook.com>
2021-02-18 17:13:07 -0800

    structure created, first js code

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Royale with Cheese", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Krusty Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Kahuna Burger", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Wendy's Double Bacon Supreme", FALSE);
