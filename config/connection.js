fcce5e822da04fb92593be8d597ca8705642b296
Michael Alexander <michael.m.alexander@outlook.com>
2021-02-22 22:10:21 -0800

    added JAWS DB

--- config/connection.js
+++ config/connection.js
@@ -19,5 +19,15 @@ var connection = mysql.createConnection({
     console.log("connected as id " + connection.threadId);
   });
 
+  if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+          host: 'localhost',
+          user: 'root',
+          password: 'ALEX00100alex!!',
+          database: 'burgers_BD'
+    });
+  };
 
 module.exports = connection;
