d3a5bd399370012d496ac35cfba87a1eda400655
Michael Alexander <michael.m.alexander@outlook.com>
2021-02-27 14:34:24 -0800

    fixed server issues

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,15 @@
 const mysql = require("mysql");
 
-const connection;
+const PORT = process.env.PORT || 3000;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'ALEX00100alex!!',
-        database: 'burgers_DB'
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3000,
+    user: "root",
+    password: "ALEX00100alex!!",
+    database: "burgers_DB"
   });
-};
-
-// var PORT = process.env.PORT || 3000;
-
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3000,
-//     user: "root",
-//     password: "ALEX00100alex!!",
-//     database: "burgers_DB"
-//   });
   
   connection.connect(function(err) {
     if (err) {
@@ -32,6 +20,16 @@ if (process.env.JAWSDB_URL) {
     console.log("connected as id " + connection.threadId);
   });
 
+  if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+          host: 'localhost',
+          user: 'root',
+          password: 'ALEX00100alex!!',
+          database: 'burgers_DB'
+    });
+  };
   
 
 module.exports = connection;
